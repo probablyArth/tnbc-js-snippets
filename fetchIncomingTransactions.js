0b1bf610dab2843a1b94dee34ede331936e29f27
Arth Thapa <arththapa7@gmail.com>
2022-01-24 09:15:46 +0530

    Update fetchIncomingTransactions.js

--- fetchIncomingTransactions.js
+++ fetchIncomingTransactions.js
@@ -3,11 +3,7 @@ const { axios } = require('axios')
 async function fetchIncomingTransactions (bankIP, accountNumber) {
   const response = await axios.get(`http://${bankIP}/bank_transactions?recipient=${accountNumber}`)
 
-  if (response.status === 200) {
-    return response.data
-  }
-
-  return 'There was an error with request at function fetchIncomingTransactions'
+  if (response.status === 200) return response.data
 }
 
 module.exports = fetchIncomingTransactions
